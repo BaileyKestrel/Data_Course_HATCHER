038c2136319a6b8f7aeda0ecd2541ba5535ec45e
Bailey Hatcher <baileyhatcher@Unknown.domain>
2025-04-12 16:32:43 -0600

    final project WIP

--- final_project/chickadee_final_project.R
+++ final_project/chickadee_final_project.R
@@ -3,44 +3,60 @@ library(ggplot2)
 library(skimr)
 library(janitor)
 
-# load in MAPS banding capture data set
-chickadee_raw <- read_csv("MAPS_BANDING_capture_data.csv")
+# load in MAPS data sets
+capture_data <- read_csv("MAPS_BANDING_capture_data.csv")
+location_data <- read_csv("MAPS_STATION_location_and_operations.csv")
+
+# merge capture_data and location_data
+joined_data <- left_join(capture_data, location_data, 
+                         by = c("LOC", "STA", "STATION"))
 
 # select columns of interest and rename columns for better understanding
-chickadee_thin <- chickadee_raw %>% 
-  select(LOC, STA, STATION, C, BAND, SPEC, AGE, HA, WRP, SEX, HS, SK, CP, BP, F, 
-         BM, FM, FW, JP, WEIGHT, STATUS, DATE, DISP, BRSTAT) %>% 
-  rename(location = LOC) %>% 
-  rename(station_num = STA) %>% 
-  rename(station_code = STATION) %>% 
-  rename(capture_code = C) %>% 
-  rename(band_num = BAND) %>% 
-  rename(species = SPEC) %>% 
-  rename(age = AGE) %>% 
-  rename(aged_how = HA) %>% 
-  rename(sex = SEX) %>% 
-  rename(sexed_how = HS) %>% 
-  rename(skull_pneum = SK) %>% 
-  rename(cloacal_prot = CP) %>% 
-  rename(broodpatch = BP) %>% 
-  rename(fat = F) %>% 
-  rename(body_molt = BM) %>% 
-  rename(flight_molt = FM) %>% 
-  rename(flight_wear = FW) %>% 
-  rename(juvenile_plum = JP) %>% 
-  rename(wing_mm = WNG) %>% 
-  rename(weight_g = WEIGHT) %>% 
-  rename(status = STATUS) %>% 
-  rename(date = DATE) %>% 
-  rename(disposition = DISP) %>% 
-  rename(breed_status = BRSTAT)
+filtered_data <- joined_data %>% 
+  select(STASLIST, LATITUDE, LONGITUDE, ELEV, HABITAT, LOC, STA, STATION, BCR, 
+         DATE, C, BAND, SPEC, AGE, HA, WRP, SEX, HS, SK, CP, BP, F, BM, FM, FW, 
+         JP, WEIGHT, WNG, STATUS, DISP, BRSTAT) %>% 
+  rename(staslist = STASLIST,
+         latitude = LATITUDE, 
+         longitude = LONGITUDE, 
+         elevation = ELEV, 
+         habitat = HABITAT, 
+         location = LOC, 
+         station_num = STA, 
+         station_code = STATION, 
+         conserv_reg = BCR, 
+         date = DATE, 
+         capture_code = C, 
+         band_num = BAND, 
+         species = SPEC, 
+         age = AGE, 
+         aged_how = HA, 
+         age_molt = WRP, 
+         sex = SEX, 
+         sexed_how = HS, 
+         skull_pneum = SK, 
+         cloacal_p = CP, 
+         broodpatch = BP, 
+         fat = F, 
+         body_molt = BM, 
+         flight_molt = FM, 
+         fliht_wear = FW, 
+         juvenal_p = JP, 
+         weight_g = WEIGHT,
+         wing_mm = WNG,
+         status = STATUS, 
+         disposition = DISP, 
+         breed_stat = BRSTAT)
+  
+  
+ 
   
 # remove rows with NA for BAND
-chickadee_thin <- chickadee_thin %>% 
+filtered_data <- filtered_data %>% 
   filter(!is.na(band_num))
 
 # create data set with just the recaptured birds
-recap_chickadee <- chickadee_thin %>% 
+recap_data <- chickadee_thin %>% 
   filter(capture_code == "R")
 
 
@@ -54,4 +70,9 @@ recap_chickadee <- chickadee_thin %>%
 
 
 
-num(unique(recap_chickadee$band_num))
+
+
+
+
+
+
